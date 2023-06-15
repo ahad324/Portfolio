63169b33191b848c985911f4a8ff06e01a2535e0
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-06-15 13:26:36 +0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -28,6 +28,7 @@ $(document).ready(function () {
 
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
         $('.navbar').toggleClass("active");
     })
 
