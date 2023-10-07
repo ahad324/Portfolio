a1bf9aa116456b35159859742060a34f5d0fc508
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-10-07 16:27:29 +0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -27,6 +27,30 @@ $(document).ready(function () {
     });
                 // <<<<<<=========================>>>>>>
     
+      // <=============== Links Smooth Scrolling  🔲 ===============>
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+
+  // <<<<<<=========================>>>>>>
     // <=============== Toggling Menu 🤓 ===============>
 
     $('.menu-btn').click(function () {
